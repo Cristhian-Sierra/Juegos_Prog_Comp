53ff0efdf3b137d2534e99887137865d95bd3b66
David Alejandro <david6462@outlook.com>
2020-11-13 19:09:04 -0500

    Prueba 2.1
    Al menos muestra algo :'v

--- app/src/main/java/com/example/juegos/Memoria.java
+++ app/src/main/java/com/example/juegos/Memoria.java
@@ -30,8 +30,8 @@ public class Memoria extends AppCompatActivity implements View.OnClickListener {
     private int teclas = 0;
     private int turno = 0;
     private String player1, player2;
-    private List<Button> letrasp1 = new ArrayList<>();
-    private List<Button> letrasp2 = new ArrayList<>();
+    private ArrayList<String> letrasp1 = new ArrayList<>();
+    private ArrayList<String> letrasp2 = new ArrayList<>();
 
 
     @Override
@@ -77,9 +77,18 @@ public class Memoria extends AppCompatActivity implements View.OnClickListener {
     public void onClick(View v) {
         teclas++;
         System.out.println(teclas);
+
         if(teclas == 1){
+            System.out.println("Entra a 1");
             game(teclas, player1, v);
         }else if (teclas == 2 || teclas == 3){
+            System.out.println("Entra a 2");
+            game(teclas, player2, v);
+        }else if (teclas == 4 || teclas == 5 || teclas == 6){
+            System.out.println("Entra a 1");
+            game(teclas, player1, v);
+        }else if(teclas == 7 || teclas == 8 || teclas == 9 || teclas == 10){
+            System.out.println("Entra a 2");
             game(teclas, player2, v);
         }
 
@@ -90,96 +99,110 @@ public class Memoria extends AppCompatActivity implements View.OnClickListener {
         if (name == player1){
             switch (view.getId()){
                 case R.id.buttonA:{
-                    letrasp1.add(buttonA);
-                    System.out.println("A");
+                    letrasp1.add("A");
+                    textViewTurno.setText(name);
                     break;
                 }
                 case R.id.buttonB:{
-                    letrasp1.add(buttonB);
-                    System.out.println("B");
+                    letrasp1.add("B");
+                    textViewTurno.setText(name);
                     break;
                 }
                 case R.id.buttonC:{
-                    letrasp1.add(buttonC);
-                    System.out.println("C");
+                    letrasp1.add("C");
+                    textViewTurno.setText(name);
                     break;
                 }
                 case R.id.buttonD:{
-                    letrasp1.add(buttonD);
-                    System.out.println("D");
+                    letrasp1.add("D");
+                    textViewTurno.setText(name);
                     break;
                 }
                 case R.id.buttonE:{
-                    letrasp1.add(buttonE);
-                    System.out.println("E");
+                    letrasp1.add("E");
+                    textViewTurno.setText(name);
                     break;
                 }
                 case R.id.buttonF:{
-                    letrasp1.add(buttonF);
-                    System.out.println("F");
+                    letrasp1.add("F");
+                    textViewTurno.setText(name);
                     break;
                 }
                 case R.id.buttonG:{
-                    letrasp1.add(buttonG);
-                    System.out.println("G");
+                    letrasp1.add("G");
+                    textViewTurno.setText(name);
                     break;
                 }
                 case R.id.buttonH:{
-                    letrasp1.add(buttonH);
-                    System.out.println("H");
+                    letrasp1.add("H");
+                    textViewTurno.setText(name);
                     break;
                 }
                 case R.id.buttonI:{
-                    letrasp1.add(buttonI);
-                    System.out.println("I");
+                    letrasp1.add("I");
+                    textViewTurno.setText(name);
                     break;
                 }
                 default:break;
-
             }
-            for (int i = 0; i<letrasp1.size(); i++){
+            System.out.print("Lista p1: ");
+            for (int i = 0; i < letrasp1.size(); i++){
                 System.out.println(letrasp1);
             }
-        }else if(name == player2){
+
+        }else{
+
             switch (view.getId()){
                 case R.id.buttonA:{
-                    letrasp2.add(buttonA);
-                    System.out.println("A");
+                    letrasp2.add("A");
+                    textViewTurno.setText(name);
                     break;
                 }
                 case R.id.buttonB:{
-                    letrasp2.add(buttonB);
+                    letrasp2.add("B");
+                    textViewTurno.setText(name);
                     break;
                 }
                 case R.id.buttonC:{
-                    letrasp2.add(buttonC);
+                    letrasp2.add("C");
+                    textViewTurno.setText(name);
                     break;
                 }
                 case R.id.buttonD:{
-                    letrasp2.add(buttonD);
+                    letrasp2.add("D");
+                    textViewTurno.setText(name);
                     break;
                 }
                 case R.id.buttonE:{
-                    letrasp2.add(buttonE);
+                    letrasp2.add("E");
+                    textViewTurno.setText(name);
                     break;
                 }
                 case R.id.buttonF:{
-                    letrasp2.add(buttonF);
+                    letrasp2.add("F");
+                    textViewTurno.setText(name);
                     break;
                 }
                 case R.id.buttonG:{
-                    letrasp2.add(buttonG);
+                    letrasp2.add("G");
+                    textViewTurno.setText(name);
                     break;
                 }
                 case R.id.buttonH:{
-                    letrasp2.add(buttonH);
+                    letrasp2.add("H");
+                    textViewTurno.setText(name);
                     break;
                 }
                 case R.id.buttonI:{
-                    letrasp2.add(buttonI);
+                    letrasp2.add("I");
+                    textViewTurno.setText(name);
                     break;
                 }
                 default:break;
+            }
+            System.out.print("Lista p2: ");
+            for (int i = 0; i < letrasp2.size(); i++){
+                System.out.println(letrasp2);
             }
         }
 
